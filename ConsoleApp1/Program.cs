8e7a2b0336945e0dcba6db846008e84d3f03ea54
JacobMarkwell <52938992+JacobMarkwell@users.noreply.github.com>
2024-01-29 19:02:39 -0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    string A = "A";
+    string B = "B";
+    string  C = "C";
+    string  D = "D";
+    string  F = "F";
+    Console.WriteLine("Please input a  Capital letter grade");
+    string grade = Console.ReadLine();
+     if(grade == A) {
+        Console.WriteLine("4");
+     }  
+     else if(grade == B){
+        Console.WriteLine("3");
+     }
+     else if(grade == C){
+        Console.WriteLine("2");
+    }
+    else if(grade == D){
+        Console.WriteLine("1");
+    }
+    else if(grade == F){
+        Console.WriteLine("0");
+    }
+  }
+
+    }
